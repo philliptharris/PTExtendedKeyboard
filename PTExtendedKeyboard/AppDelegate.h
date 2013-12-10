749da456fe8a63a27ff31691f38a71e0c6a79475
Phillip Harris <phillip.harris@atlanticbt.com>
2013-12-10 09:41:06 -0500

    Initial Commit

---
+++ PTExtendedKeyboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PTExtendedKeyboard
+//
+//  Created by Phillip Harris on 12/10/13.
+//  Copyright (c) 2013 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
